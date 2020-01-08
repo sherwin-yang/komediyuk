b5c00c993251e92e43e8500eefdc621b35470b2f
dika <andikarookie@gmail.com>
2020-01-08 19:41:23 +0700

    model video sama komika

--- app/Komika.php
+++ app/Komika.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Komika extends Model
 {
-    //
+    public function video(){
+        return $this->hasMany(video::class);
+    }
+
+    public function tiekt(){
+        return $this->hasMany(Tiket::class);
+    }
 }
--- app/Video.php
+++ app/Video.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
-    //
+    public function komika(){
+        return $this->belongsTo(Komika::class);
+    }
 }
